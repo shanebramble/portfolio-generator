ce614e98f694784cf818d961b1a66189f1b44a97
Shane Bramble-Wade <sbramwade@msn.com>
2021-08-16 10:07:58 -0400

    included inquirer module and methods

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+// const pageHTML = generatePage (name,github);
 
-// ES6 feature called assignment destructuring
-// In basic terms, it assigns elements of an array to variable names in a single expression, as shown here:
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([{
+        type: "input",
+        name:"name",
+        message: "What is your name?"
+    }
+])
+.then(answers => console.log(answers));
